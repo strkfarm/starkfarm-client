3efa53b29366b42bc9338ad9442fe4133773affa
zkdice-akira <zkdice@proton.me>
2024-03-15 23:41:33 +0530

    fix hashstack apr

--- src/store/hashstack.store.ts
+++ src/store/hashstack.store.ts
@@ -12,6 +12,52 @@ export class Hashstack extends ZkLend {
     logo = 'https://app.hashstack.finance/favicon-32x32.png'
 
     incentiveDataKey = 'Hashstack'
+    SUPPLY_FACTOR = 0.7
+    _computePoolsInfo(data: any) {
+        const myData = data[this.incentiveDataKey];
+        if (!myData) return [];
+        const pools: PoolInfo[] = [];
+        Object.keys(myData).forEach(poolName => {
+            const arr = myData[poolName];
+            if (arr.length == 0) return;
+
+            let category = Category.Others;
+            if (['USDC', 'USDT'].includes(poolName)) {
+                category = Category.Stable
+            } else if (poolName.includes('STRK')) {
+                category = Category.STRK;
+            }
+            
+            const logo1 = CONSTANTS.LOGOS[<TokenName>poolName];
+
+            const poolInfo: PoolInfo = {
+                pool: {
+                    name: poolName,
+                    logos: [logo1]
+                }, protocol: {
+                    name: this.name,
+                    link: this.link,
+                    logo: this.logo,
+                },
+                apr: arr[arr.length - 1].strk_grant_apr_nrs * this.SUPPLY_FACTOR,
+                tvl: arr[arr.length - 1].supply_usd,
+                aprSplits: [{
+                    apr: 0,
+                    title: 'Base APR',
+                    description: 'Shown soon',
+                }, {
+                    apr: arr[arr.length - 1].strk_grant_apr_nrs * this.SUPPLY_FACTOR,
+                    title: 'STRK rewards',
+                    description: 'Starknet DeFi Spring incentives',
+                }],
+                category: category,
+                type: PoolType.Lending
+            }
+            pools.push(poolInfo);
+        })
+        
+        return pools;
+    }
 }
 
 
