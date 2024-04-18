c54ec14ea55c26a51155eb9198ffda0200322d7b
Catherine Jonathan <35270183+kateberryd@users.noreply.github.com>
2024-04-18 11:15:23 +0100

    fix: resolved claim page linting error

--- src/app/claims/page.tsx
+++ src/app/claims/page.tsx
@@ -1,62 +1,57 @@
 "use client";
-
-import Navbar from "@/components/Navbar";
-import EkuboAtoms from "@/store/ekobu.store";
-import Ekubo from "@/store/ekobu.store";
-import Jediswap from "@/store/jedi.store";
 import {
-	Avatar,
-	Button,
-	Card,
-	Container,
-	Flex,
-	Grid,
-	GridItem,
-	Heading,
-	Stat,
-	StatLabel,
-	StatNumber,
+  Avatar,
+  Button,
+  Card,
+  Container,
+  Flex,
+  Grid,
+  GridItem,
+  Heading,
+  Stat,
+  StatLabel,
+  StatNumber,
 } from "@chakra-ui/react";
 import CONSTANTS from "@/constants";
 import useClaimReward from "@/hooks/useClaimReward";
 
 export default function Claim() {
-	const { handleClaimReward } = useClaimReward();
+  const { handleClaimReward } = useClaimReward();
 
-	return (
-		<Container maxWidth={"1000px"} margin={"0 auto"} padding="30px 10px">
-			<Heading as="h2" color="white" marginBottom={"10px"}>
-				<Avatar marginRight={"5px"} src={CONSTANTS.LOGOS.STRK} />
-				Claim $STRK Tokens
-			</Heading>
+  return (
+    <Container maxWidth={"1000px"} margin={"0 auto"} padding="30px 10px">
+      <Heading as="h2" color="white" marginBottom={"10px"}>
+        <Avatar marginRight={"5px"} src={CONSTANTS.LOGOS.STRK} />
+        Claim $STRK Tokens
+      </Heading>
 
-			<Grid templateColumns="repeat(3, 1fr)" gap="6">
-				<GridItem display="flex">
-					<Card width="100%" padding={"15px 30px"} color="white" bg="highlight">
-						<Stat>
-							<StatLabel>Total Earned</StatLabel>
-							<StatNumber>0</StatNumber>
-						</Stat>
-					</Card>
-				</GridItem>
-				<GridItem display="flex">
-					<Card width="100%" padding={"15px 30px"} color="white" bg="highlight">
-						<Stat>
-							<StatLabel>Total Claimed</StatLabel>
-							<StatNumber>0</StatNumber>
-						</Stat>
-					</Card>
-				</GridItem>
-				<GridItem display="flex">
-					<Card width="100%" padding={"15px 30px"} color="white" bg="highlight">
-						<Stat>
-							<StatLabel>Unclaimed</StatLabel>
-							<StatNumber>0</StatNumber>
-						</Stat>
-						<Button onClick={handleClaimReward}>Claim</Button>
-					</Card>
-				</GridItem>
-			</Grid>
-		</Container>
-	);
+      <Grid templateColumns="repeat(3, 1fr)" gap="6">
+        <GridItem display="flex">
+          <Card width="100%" padding={"15px 30px"} color="white" bg="highlight">
+            <Stat>
+              <StatLabel>Total Earned</StatLabel>
+              <StatNumber>0</StatNumber>
+            </Stat>
+          </Card>
+        </GridItem>
+        <GridItem display="flex">
+          <Card width="100%" padding={"15px 30px"} color="white" bg="highlight">
+            <Stat>
+              <StatLabel>Total Claimed</StatLabel>
+              <StatNumber>0</StatNumber>
+            </Stat>
+          </Card>
+        </GridItem>
+        <GridItem display="flex">
+          <Card width="100%" padding={"15px 30px"} color="white" bg="highlight">
+            <Stat>
+              <StatLabel>Unclaimed</StatLabel>
+              <StatNumber>0</StatNumber>
+            </Stat>
+            <Button onClick={handleClaimReward}>Claim</Button>
+          </Card>
+        </GridItem>
+      </Grid>
+    </Container>
+  );
 }
