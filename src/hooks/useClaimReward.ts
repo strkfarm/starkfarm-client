2d29df82f90649a626770df9c0d62224e6cddcf6
Catherine Jonathan <35270183+kateberryd@users.noreply.github.com>
2024-04-18 11:19:01 +0100

    fix: resolved claim page linting error

--- src/hooks/useClaimReward.ts
+++ src/hooks/useClaimReward.ts
@@ -6,39 +6,39 @@ import { useProvider } from "@starknet-react/core";
 import { getContractABI } from "@/utils/ABI";
 
 export default function useClaimReward() {
-	const { address } = useAccount();
-	const [{ mutate, status, data }] = useAtom(zklendAtom);
-	const { provider } = useProvider();
+  const { address } = useAccount();
+  const [{ mutate, status, data }] = useAtom(zklendAtom);
+  const { provider } = useProvider();
 
-	useEffect(() => {
-		if (data && status) {
-			const claimContracts = data.map((item: any) => item.claim_contract);
+  useEffect(() => {
+    if (data && status) {
+      const claimContracts = data.map((item: any) => item.claim_contract);
 
-			if (claimContracts.length > 0) {
-				const fetchData = async () => {
-					try {
-						const contractABIs = await getContractABI({
-							provider: provider,
-							address: claimContracts[0],
-						});
+      if (claimContracts.length > 0) {
+        const fetchData = async () => {
+          try {
+            const contractABIs = await getContractABI({
+              provider: provider,
+              address: claimContracts[0],
+            });
 
-						console.log(contractABIs, "contract ABI")
-						
-					} catch (error) {
-						console.error("Error:", error);
-					}
-				};
+            console.log(contractABIs, "contract ABI")
+            
+          } catch (error) {
+            console.error("Error:", error);
+          }
+        };
 
-				fetchData();
-			}
-		}
-	}, [data, status]);
+        fetchData();
+      }
+    }
+  }, [data, status]);
 
-	const handleClaimReward = () => {
-		mutate({ address });
-	};
+  const handleClaimReward = () => {
+    mutate({ address });
+  };
 
-	return {
-		handleClaimReward,
-	};
+  return {
+    handleClaimReward,
+  };
 }
