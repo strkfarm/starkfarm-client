505634d70487a429ff42e67e5e53304c4978649d
Catherine Jonathan <35270183+kateberryd@users.noreply.github.com>
2024-04-18 11:50:46 +0100

    fix: update getContractABI to retrieve ABI for all claim_contracts and return an array

--- src/hooks/useClaimReward.ts
+++ src/hooks/useClaimReward.ts
@@ -4,41 +4,53 @@ import { useAtom } from "jotai";
 import { useEffect, useState } from "react";
 import { useProvider } from "@starknet-react/core";
 import { getContractABI } from "@/utils/ABI";
+import { Console } from "console";
 
 export default function useClaimReward() {
-  const { address } = useAccount();
-  const [{ mutate, status, data }] = useAtom(zklendAtom);
-  const { provider } = useProvider();
+	const { address } = useAccount();
+	const [{ mutate, status, data }] = useAtom(zklendAtom);
+	const { provider } = useProvider();
 
-  useEffect(() => {
-    if (data && status) {
-      const claimContracts = data.map((item: any) => item.claim_contract);
+	useEffect(() => {
+		if (data && status) {
+			const claimContracts = data.map((item: any) => item.claim_contract);
 
-      if (claimContracts.length > 0) {
-        const fetchData = async () => {
-          try {
-            const contractABIs = await getContractABI({
-              provider: provider,
-              address: claimContracts[0],
-            });
+			if (claimContracts.length > 0) {
+				const fetchData = async () => {
+					try {
+						// Fetch contract ABIs for all contracts in claimContracts
+						const contractABIs = await Promise.all(
+							claimContracts.map(async (contractAddress: string) => {
+								try {
+									return await getContractABI({
+										provider: provider,
+										address: contractAddress,
+									});
+								} catch (error) {
+									console.error(
+										"Error fetching ABI for contract address:",
+										contractAddress,
+										error
+									);
+									return null; // Return null for failed fetches
+								}
+							})
+						);
+						console.log(contractABIs, "contract");
+					} catch (error) {
+						console.error("Error:", error);
+					}
+				};
+				fetchData();
+			}
+		}
+	}, [data, status]);
 
-            console.log(contractABIs, "contract ABI")
-            
-          } catch (error) {
-            console.error("Error:", error);
-          }
-        };
+	const handleClaimReward = () => {
+		mutate({ address });
+	};
 
-        fetchData();
-      }
-    }
-  }, [data, status]);
-
-  const handleClaimReward = () => {
-    mutate({ address });
-  };
-
-  return {
-    handleClaimReward,
-  };
+	return {
+		handleClaimReward,
+	};
 }
