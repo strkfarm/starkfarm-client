5f2fd5b5d4ffbd855a443e7223692e72595e7193
Fishon Amos <43862685+fishonamos@users.noreply.github.com>
2024-06-10 09:21:18 +0000

    constants

--- src/constants.ts
+++ src/constants.ts
@@ -32,9 +32,11 @@ const CONSTANTS = {
     BASE_APR_API: '/ekubo/pair',
     BASE_PRICE_API: '/ekubo/price',
   },
+
   MYSWAP: {
-    BASE_APR_API: '/#/pair',
+    BASE_APR_API: '/#/positions',
     BASE_PRICE_API: '/#/price',
+    
   },
   CONTRACTS: {
     Master: '0x50314707690c31597849ed66a494fb4279dc060f8805f21593f52906846e28e',
