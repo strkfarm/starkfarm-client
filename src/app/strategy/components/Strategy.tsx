c8820d1d99a38ef81f87b1f02804be86df7fa030
akira-zkdice <zkdice@proton.me>
2024-07-19 14:15:09 +0530

    formatting fix

--- src/app/strategy/components/Strategy.tsx
+++ src/app/strategy/components/Strategy.tsx
@@ -268,7 +268,8 @@ const Strategy = () => {
               Behind the scenes
             </Text>
             <Text fontSize={'15px'} marginBottom={'10px'}>
-              Actions done automatically by the strategy (smart-contract) with an investment of $1000
+              Actions done automatically by the strategy (smart-contract) with
+              an investment of $1000
             </Text>
             <Flex
               color="white"
