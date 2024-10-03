d7e60a553294730ae0c0963a4ca93352f76c57fe
Akira <156126180+akiraonstarknet@users.noreply.github.com>
2024-10-03 19:49:10 +0530

    Update sensei limits

--- src/store/strategies.atoms.ts
+++ src/store/strategies.atoms.ts
@@ -49,7 +49,7 @@ export function getStrategies() {
     [1, 0.615384615, 1, 0.584615385, 0.552509024], // precomputed factors based on strategy math
     StrategyLiveStatus.NEW,
     {
-      maxTVL: 500000,
+      maxTVL: 1500000,
     },
   );
 
@@ -62,7 +62,7 @@ export function getStrategies() {
     [1, 0.609886, 1, 0.920975, 0.510078], // precomputed factors based on strategy math
     StrategyLiveStatus.NEW,
     {
-      maxTVL: 100,
+      maxTVL: 500,
     },
   );
   const deltaNeutralMMSTRKETH = new DeltaNeutralMM(
@@ -74,7 +74,7 @@ export function getStrategies() {
     [1, 0.384615, 1, 0.492308, 0.233276], // precomputed factors based on strategy math, last is the excess deposit1 that is happening
     StrategyLiveStatus.NEW,
     {
-      maxTVL: 500000,
+      maxTVL: 1500000,
     },
   );
 
